5e35073419a51dd7007fc6ed0eebeab02a63675a
Andrey Sukhovev <andrei5s@yandex.ru>
2023-02-22 18:00:02 +0300

    Дипломный проект

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-mongoose.connect('mongodb://localhost:27017/moviesesdb');
+mongoose.connect('mongodb://localhost:27017/movies1db');
 
 // для собирания JSON-формата
 app.use(bodyParser.json());
